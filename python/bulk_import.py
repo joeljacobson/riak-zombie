9092ace515899f2afeff36ef49aa8727c5d0a5d0
Richard Shaw <richard@basho.com>
2013-09-01 19:25:47 +0100

    corrections

--- python/bulk_import.py
+++ python/bulk_import.py
@@ -5,7 +5,7 @@ import riak
 client = riak.RiakClient(pb_port=8087, protocol='pbc')
 bucket = client.bucket('za')
 
-f = open("ZA10K.csv", "r")
+f = open("ZA10k.csv", "r")
 for row in csv.DictReader(f):
     key = row['social'] # Dates are unique, use as keys.
     item = bucket.new(key, data=row)
